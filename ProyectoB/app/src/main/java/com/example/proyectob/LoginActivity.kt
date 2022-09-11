eab9eea118def2442b9fd38ae6882920002ae6e6
JonyStrins <slipknot1852@gmail.com>
2022-09-11 14:28:50 -0500

    commit init

--- ProyectoB/app/src/main/java/com/example/proyectob/LoginActivity.kt
+++ ProyectoB/app/src/main/java/com/example/proyectob/LoginActivity.kt
@@ -3,9 +3,11 @@ package com.example.proyectob
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.result.ActivityResultCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity(){
@@ -17,10 +19,21 @@ class LoginActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_login)
 
-        actResultLauncher = registerForActivityResult(ActivityResultContracts.StarActivityForResult(),
-            ActivityResultCallback {
 
-            })
+        actResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback {
+                if (it.resultCode == RESULT_OK){
+                    val intpar = it.data
+                    val email = intpar?.getStringExtra("email")
+
+                    Toast.makeText(applicationContext,
+                        "email: $email",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        )
 
         btnSignin = findViewById(R.id.btnSignin)
         btnSignin.setOnClickListener {
@@ -37,7 +50,8 @@ class LoginActivity : AppCompatActivity(){
                     this.putExtra("param1", 0)
                     this.putExtra("otropar", "Register")
                 }
-            startActivity(int_act_signin)
+            // startActivity(int_act_signin)
+           // actResultLauncher.launch()
         }
     }
 
--- ProyectoB/app/src/main/java/com/example/proyectob/SigninActivity.kt
+++ ProyectoB/app/src/main/java/com/example/proyectob/SigninActivity.kt
@@ -1,10 +1,19 @@
 package com.example.proyectob
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class SigninActivity : AppCompatActivity() {
+
+    lateinit var txtN : EditText
+    lateinit var txtE : EditText
+    lateinit var btnSave : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
@@ -15,5 +24,16 @@ class SigninActivity : AppCompatActivity() {
 
         val accion = intent.getIntExtra("param1", -1)
         Toast.makeText(applicationContext, "Accion: $accion", Toast.LENGTH_LONG).show()
+
+        txtN = findViewById(R.id.txtName)
+        txtE = findViewById(R.id.txtEmail)
+        btnSave = findViewById(R.id.btnSignin)
+
+        btnSave.setOnClickListener{
+            val intentRes = Intent()
+            intentRes.putExtra("email", txtE.text.toString())
+            setResult(RESULT_OK, intentRes)
+            finish()
+        }
     }
 }
